aaea4f95d2382f8450807275664b21f8426029c7
Alicia Vallorani <auv27@psu.edu>
2019-08-21 10:24:00 -0400

    omit missing data
    making visualization easier by omitting missing data

--- R/load_spi.R
+++ R/load_spi.R
@@ -8,4 +8,5 @@ spi <- spi %>%
          wellness=factor(wellness, levels=c(1,2), labels=c("Low", "High")),
          exercise=factor(exercise, levels=c(1,2,3), labels=c("Rarely", "Sometimes", "Often")),
          wellness=factor(wellness, levels=c(1,2), labels=c("Low", "High")),
-         ER=factor(ER, levels=c(1,2,3,4), labels=c("None", "1x", "2x", "3+")))
+         ER=factor(ER, levels=c(1,2,3,4), labels=c("None", "1x", "2x", "3+"))) %>%
+  na.omit()
