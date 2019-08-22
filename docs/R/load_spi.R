22cae017dba9bfc304d94ecb41e762557d97f64b
rogilmore <thatrickgilmore@gmail.com>
2019-08-22 04:40:42 -0400

    Add links to data_visualization

---
+++ docs/R/load_spi.R
@@ -0,0 +1,12 @@
+require(dplyr)
+data("spi", package="psych")
+spi <- spi %>% 
+  renmame(exercise = exer) %>%
+  dplyr::select(age, sex, health, p1edu, p2edu, education, wellness,
+                exer, smoke, ER) %>%
+  mutate(sex=factor(sex, levels=c(1,2), labels=c("male", "female")),
+         wellness=factor(wellness, levels=c(1,2), labels=c("Low", "High")),
+         exercise=factor(exercise, levels=c(1,2,3), labels=c("Rarely", "Sometimes", "Often")),
+         wellness=factor(wellness, levels=c(1,2), labels=c("Low", "High")),
+         ER=factor(ER, levels=c(1,2,3,4), labels=c("None", "1x", "2x", "3+"))) %>%
+  na.omit()
