fd13fd8201156abe080a73d9b0d7f06f360d5b4e
rogilmore <thatrickgilmore@gmail.com>
2019-08-22 02:20:01 -0400

    manage_packages, copy_rmd_2_box

--- R/manage_packages.R
+++ R/manage_packages.R
@@ -10,55 +10,57 @@ bootcamp_pkgs <- c("afex",
                    "emmeans",
                    "ez",
                    "ggfortify",
-                   "ggplot2",
                    "googlesheets", 
                    "googledrive",
                    "Hmisc",
                    "interactions",
                    "knitr",
+                   "lavaan",
                    "lme4",
                    "lmerTest",
                    "jtools",
                    "plyr",
                    "psych",
                    "rcompanion",
+                   "Rtools",
                    "skimr",
                    "swirl",
                    "tidyverse",
                    "tufte")
 
-install_bootcamp_pkgs <- function(pkgs = bootcamp_pkgs) {
-  install_packages(bootcamp_pkgs)
+install_bootcamp_pkgs <- function(pkgs = bootcamp_pkgs, install_message = TRUE,
+                                  force_reinstall = FALSE) {
+  install_pkgs(bootcamp_pkgs, install_message, force_reinstall)
 }
 
 test_bootcamp_pkg_install <- function() {
-  if (install_packages()) {
-    message("Test passes")
+  if (!is.null(install_pkgs(c("ggplot2", "dplyr")))) {
+    message("Test fails.")
+  } else {
+    message("Test passes.")
   }
 }
 
-install_pacman <- function() {
-  if (!require(pacman)) {
-    message("Installing 'pacman' package")
-    install.packages("pacman")
-  }
-  library(pacman)
-}
-
-install_install.load <- function() {
-  if (!require(install.load)) {
-    message("Installing 'install.load' package from CRAN")
-    install.packages("install.load")
-  }
-  library(install.load)
-}
-
-install_packages <- function(pkg_list = c("tidyverse", "ggplot2")) {
-  install_install.load()
-  if (!is.null(pkg_list)) {
-    results <- lapply(pkg_list, install_load)
-    } else {
-      stop("'pkg_list' is NULL.")
+install_pkg <- function(pkg, install_message = FALSE, force_reinstall = FALSE) {
+  if (force_reinstall) {
+    if (install_message) {
+      message(paste0("Reinstalling package: '", pkg, "'\n"))
     }
-  return(TRUE)
+    install.packages(pkg, quiet = TRUE, verbose = FALSE)
+  } else {
+    if(require(pkg, character.only = TRUE, quietly = TRUE)) {
+      if (install_message) {
+        message(paste0("Package '", pkg, "': already installed.\n"))      
+      }
+    } else {
+      if (install_message) {
+        message(paste0("Installing package: '", pkg, "'\n"))
+      }
+      install.packages(pkg, quiet = TRUE, verbose = FALSE)
+    }
+  }
+}
+
+install_pkgs <- function(pkgs, install_message = FALSE, force_reinstall = FALSE) {
+  unlist(lapply(pkgs, install_pkg, install_message, force_reinstall))
 }
